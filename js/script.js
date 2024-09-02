a8cb38fe64f7f514506dbe22002956992b361599
Felipe Jacobo <Felipe09878@gmail.com>
2024-09-02 16:31:15 -0700

    Micommit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function search() {
+    // Obtiene el valor del input de búsqueda con el id 'searchInput'
+    const query = document.getElementById('searchInput').value;
+    
+    // Muestra una alerta con el término de búsqueda ingresado
+    alert(`Buscando: ${query}`);
+    
+}
